71efe391a8c4eeb136fc0e057ea4bf60f5006e19
Ubuntu <mac@c1-cp1.liiuk5qm411unisvtdt1hlwxqf.bx.internal.cloudapp.net>
2022-04-22 15:29:14 +0000

    tag is parameterized

--- push.sh
+++ push.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 rm -rf cd
 git clone https://github.com/XerO00/cd.git
 cd cd/
-ls
-sed "s/tagVersion/2.0/g" deployment.yaml  > deployment.yaml
+sed "s/tagVersion/$1/g" tdeployment.yaml > deployment.yaml
 cat deployment.yaml
 git add .
-git commit -m 'tag changed'
-git status
+git commit -m "tag changed"
+git push https://${GIT_USER_NAME}:${GIT_PWD}@github.com/XerO00/cd.git
