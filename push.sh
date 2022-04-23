dea76ed73a0cad17f23c4cd7f7edbe44725c49b2
Ubuntu <mac@c1-cp1.liiuk5qm411unisvtdt1hlwxqf.bx.internal.cloudapp.net>
2022-04-23 12:44:31 +0000

    push  file update

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 rm -rf cd
 git clone https://github.com/XerO00/cd.git
 cd cd/
-sed "/^\([[:space:]]*tag: \).*/s//\1${TAG}/g" -i values.yaml
+sed "/^\([[:space:]]*tag: \).*/s//\1${TAG}/g" -i /helm/chart/values.yaml
 git add .
 git commit -m "tag version updated"
 git push https://${GIT_USER_NAME}:${GIT_PWD}@github.com/XerO00/cd.git
